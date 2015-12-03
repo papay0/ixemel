4d25db0c0ee1a48437a334129aac95af125972e2
Arthur Papailhau <papailhau@gmail.com>
2015-12-03 16:22:22 +0100

    Add admin panel

--- js/site.js
+++ js/site.js
@@ -1,3 +1,11 @@
 /**
- * Created by gautier on 20/11/15.
+ * Created by gautier and Arthur on 20/11/15.
  */
+
+ $(function() {
+ 	$('#studentsByCursus').click(function() {
+ 		$.get('html/studentsPerGroup.html', function(data) {
+ 			$('.content').html(data);
+ 		});    
+ 	});  
+ });

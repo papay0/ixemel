58795e8be7d914e90b903681256ef52a2cebcb33
Arthur Papailhau <papailhau@gmail.com>
2015-12-03 19:01:10 +0100

    add stat main admin

--- js/site.js
+++ js/site.js
@@ -9,4 +9,32 @@
  			$('.content').html(data);
  		});    
  	});  
+
+ 	$.ajax( {
+ 		type: "GET",
+ 		url: "xml/project.xml",
+ 		dataType: "xml",
+ 		success: function(xml) { 
+ 			var numberOfStudents = 0;
+ 			var numberOfSubjects = 0;
+ 			var numberOfTeachers = 0;
+ 			var numberOfSubjectsAtLeastOneDebt = 0;
+
+ 			$(xml).find('student').each( function(){ 
+ 				numberOfStudents++;
+ 			} );
+ 			$(xml).find('subjects:first').find('subject').each( function(){ 
+ 				numberOfSubjects++;
+ 			} );
+ 			$(xml).find('teacher').each( function(){ 
+ 				numberOfTeachers++;
+ 			} );
+
+ 			$('#numberOfStudents').text(numberOfStudents);
+ 			$('#numberOfSubjects').text(numberOfSubjects);
+ 			$('#numberOfTeachers').text(numberOfTeachers);
+ 			$('#numberOfSubjectsAtLeastOneDebt').html(numberOfSubjectsAtLeastOneDebt+'<sup style="font-size: 20px">%</sup>');
+ 		}
+ 	}
+ 	); 
  });

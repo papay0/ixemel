b2489f215e16c913b72cc355232962b6462e780c
Arthur Papailhau <papailhau@gmail.com>
2015-12-10 13:05:59 +0100

    Revert "update js"
    This reverts commit 8243cda310c457921e4e2ae1f3e85a186bcda25d.

--- js/site.js
+++ js/site.js
@@ -71,7 +71,6 @@
  	});
 
  	$('#studentsDebtsPreviousYear').click(function() {
- 		changeText('Étudiants ayant des dettes les années précédentes');
  		$.get('html/studentsDebtsPreviousYear.html', function(data) {
  			$('.content').html(data);
  		});    

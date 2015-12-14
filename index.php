77a47df6ee9f8fa2e55296e10744f01267bb02f8
gautierdelorme <gautier.delorme@gmail.com>
2015-12-14 11:23:59 +0100

    remove unused tab

--- index.php
+++ index.php
@@ -406,46 +406,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
 		<aside class="control-sidebar control-sidebar-dark">
 			<!-- Create the tabs -->
 			<ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-				<li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-				<li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+				<li class="active"><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
 			</ul>
 			<!-- Tab panes -->
 			<div class="tab-content">
-				<!-- Home tab content -->
-				<div class="tab-pane active" id="control-sidebar-home-tab">
-					<h3 class="control-sidebar-heading">Recent Activity</h3>
-					<ul class="control-sidebar-menu">
-						<li>
-							<a href="javascript::;">
-								<i class="menu-icon fa fa-birthday-cake bg-red"></i>
-								<div class="menu-info">
-									<h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-									<p>Will be 23 on April 24th</p>
-								</div>
-							</a>
-						</li>
-					</ul><!-- /.control-sidebar-menu -->
-
-					<h3 class="control-sidebar-heading">Tasks Progress</h3>
-					<ul class="control-sidebar-menu">
-						<li>
-							<a href="javascript::;">
-								<h4 class="control-sidebar-subheading">
-									Finir ce site.
-									<span class="label label-danger pull-right">100%</span>
-								</h4>
-								<div class="progress progress-xxs">
-									<div class="progress-bar progress-bar-danger" style="width: 100%"></div>
-								</div>
-							</a>
-						</li>
-					</ul><!-- /.control-sidebar-menu -->
-
-				</div><!-- /.tab-pane -->
-				<!-- Stats tab content -->
-				<div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div><!-- /.tab-pane -->
-				<!-- Settings tab content -->
-				<div class="tab-pane" id="control-sidebar-settings-tab">
+				<div class="tab-pane active" id="control-sidebar-settings-tab">
 					<form method="POST">
 						<h3 class="control-sidebar-heading">General Settings</h3>
 						<div class="form-group">

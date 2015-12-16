df735d54bf6ba610350c82fdd1f96bc70282588a
gautierdelorme <gautier.delorme@gmail.com>
2015-12-16 11:08:54 +0100

    remove unused notifications menu

--- index.php
+++ index.php
@@ -118,28 +118,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 							</ul>
 						</li><!-- /.messages-menu -->
 
-						<!-- Notifications Menu -->
-						<li class="dropdown notifications-menu">
-							<!-- Menu toggle button -->
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-								<i class="fa fa-bell-o"></i>
-								<span class="label label-warning">1</span>
-							</a>
-							<ul class="dropdown-menu">
-								<li class="header">Vous avez 1 notification</li>
-								<li>
-									<!-- Inner Menu: contains the notifications -->
-									<ul class="menu">
-										<li><!-- start notification -->
-											<a href="#">
-												<i class="fa fa-users text-aqua"></i> 5 nouveaux membres vous ont join aujourd'hui
-											</a>
-										</li><!-- end notification -->
-									</ul>
-								</li>
-								<li class="footer"><a href="#">View all</a></li>
-							</ul>
-						</li>
 						<!-- Tasks Menu -->
 						<li class="dropdown tasks-menu">
 							<!-- Menu Toggle Button -->

7133ac9c5242fbdf75947c333b39854c4b6c5c32
gautierdelorme <gautier.delorme@gmail.com>
2015-12-17 09:55:49 +0100

    remove comments

--- index.php
+++ index.php
@@ -5,89 +5,33 @@ if (isset($_POST['button']))
 }
 ?>
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>IXEMEL</title>
-	<!-- Tell the browser to be responsive to screen width -->
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-	<!-- Bootstrap 3.3.5 -->
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<!-- Font Awesome -->
-	<!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
-	<!-- Ionicons -->
-	
-	<!-- Theme style -->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">	
 	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/ionicons.min.css">
-    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
-          page. However, you can choose any other skin. Make sure you
-          apply the skin class to the body tag so the changes take effect.
-      -->
-      <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css">
-
-      <!-- CSS PERSO -->
-      <link rel="stylesheet" href="css/style.css">
-
-      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    </head>
-  <!--
-  BODY TAG OPTIONS:
-  =================
-  Apply one or more of the following classes to get the
-  desired effect
-  |---------------------------------------------------------|
-  | SKINS         | skin-blue                               |
-  |               | skin-black                              |
-  |               | skin-purple                             |
-  |               | skin-yellow                             |
-  |               | skin-red                                |
-  |               | skin-green                              |
-  |---------------------------------------------------------|
-  |LAYOUT OPTIONS | fixed                                   |
-  |               | layout-boxed                            |
-  |               | layout-top-nav                          |
-  |               | sidebar-collapse                        |
-  |               | sidebar-mini                            |
-  |---------------------------------------------------------|
--->
+    <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
 <body class="hold-transition skin-blue sidebar-mini">
 	<div class="wrapper">
-
-		<!-- Main Header -->
 		<header class="main-header">
-
-			<!-- Logo -->
 			<a href="./" class="logo">
-				<!-- mini logo for sidebar mini 50x50 pixels -->
 				<span class="logo-mini"><b>X</b>ML</span>
-				<!-- logo for regular state and mobile devices -->
 				<span class="logo-lg"><b>IXEMEL</b></span>
 			</a>
-
-			<!-- Header Navbar -->
 			<nav class="navbar navbar-static-top" role="navigation">
-				<!-- Sidebar toggle button-->
 				<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
 					<span class="sr-only">Toggle navigation</span>
 				</a>
-				<!-- Navbar Right Menu -->
 				<div class="navbar-custom-menu">
 					<ul class="nav navbar-nav">
-						<!-- Messages: style can be found in dropdown.less-->
 						<li class="dropdown messages-menu">
-							<!-- Menu toggle button -->
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 								<i class="fa fa-envelope-o"></i>
 								<span class="label label-success">1 </span>
@@ -95,20 +39,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
 							<ul class="dropdown-menu">
 								<li class="header">You have 1 message</li>
 								<li>
-									<!-- inner menu: contains the messages -->
 									<ul class="menu">
-										<li><!-- start message -->
+										<li>
 											<a href="#">
 												<div class="pull-left">
-													<!-- User Image -->
 													<img src="resources/BG_du_XML.jpg" class="img-circle" alt="User Image">
 												</div>
-												<!-- Message title and timestamp -->
 												<h4>
 													Support Team
 													<small><i class="fa fa-clock-o"></i> 5 mins</small>
 												</h4>
-												<!-- The message -->
 												<p>Embauche Google: Arthur & Gautier</p>
 											</a>
 										</li><!-- end message -->
@@ -118,9 +58,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 							</ul>
 						</li><!-- /.messages-menu -->
 
-						<!-- Tasks Menu -->
 						<li class="dropdown tasks-menu">
-							<!-- Menu Toggle Button -->
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 								<i class="fa fa-flag-o"></i>
 								<span class="label label-danger">3</span>
@@ -128,56 +66,46 @@ scratch. This page gets rid of all links and provides the needed markup only.
 							<ul class="dropdown-menu">
 								<li class="header">Vous avez 3 taches</li>
 								<li>
-									<!-- Inner menu: contains the tasks -->
 									<ul class="menu">
-										<li><!-- Task item -->
+										<li>
 											<a href="#">
-												<!-- Task title and progress text -->
 												<h3>
 													Finir le code de Bastien Micheau
 													<small class="pull-right">80%</small>
 												</h3>
-												<!-- The progress bar -->
 												<div class="progress xs">
-													<!-- Change the css width attribute to simulate progress -->
 													<div class="progress-bar progress-bar-aqua" style="width: 80%" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
 														<span class="sr-only">80% Complete</span>
 													</div>
 												</div>
 											</a>
-										</li><!-- end task item -->
-										<li><!-- Task item -->
+										</li>
+										<li>
 											<a href="#">
-												<!-- Task title and progress text -->
 												<h3>
 													Expliquer Git à Bastien Micheau
 													<small class="pull-right">20%</small>
 												</h3>
-												<!-- The progress bar -->
 												<div class="progress xs">
-													<!-- Change the css width attribute to simulate progress -->
 													<div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
 														<span class="sr-only">20% Complete</span>
 													</div>
 												</div>
 											</a>
-										</li><!-- end task item -->
-										<li><!-- Task item -->
+										</li>
+										<li>
 											<a href="#">
-												<!-- Task title and progress text -->
 												<h3>
 													Finir ce site.
 													<small class="pull-right">100%</small>
 												</h3>
-												<!-- The progress bar -->
 												<div class="progress xs">
-													<!-- Change the css width attribute to simulate progress -->
 													<div class="progress-bar progress-bar-aqua" style="width: 100%" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
 														<span class="sr-only">100% Complete</span>
 													</div>
 												</div>
 											</a>
-										</li><!-- end task item -->
+										</li>
 									</ul>
 								</li>
 								<li class="footer">
@@ -185,17 +113,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
 								</li>
 							</ul>
 						</li>
-						<!-- User Account Menu -->
 						<li class="dropdown user user-menu">
-							<!-- Menu Toggle Button -->
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-								<!-- The user image in the navbar-->
 								<img src="resources/BG_du_XML.jpg" class="user-image" alt="User Image">
-								<!-- hidden-xs hides the username on small devices so only the image appears. -->
 								<span class="hidden-xs">Arthur & Gautier</span>
 							</a>
 							<ul class="dropdown-menu">
-								<!-- The user image in the menu -->
 								<li class="user-header">
 									<img src="resources/BG_du_XML.jpg" class="img-circle" alt="User Image">
 									<p>
@@ -203,7 +126,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 										<small>Member since Nov. 2015</small>
 									</p>
 								</li>
-								<!-- Menu Body -->
 								<li class="user-body">
 									<div class="col-xs-4 text-center">
 										<a href="#">Followers</a>
@@ -215,7 +137,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 										<a href="#">Friends</a>
 									</div>
 								</li>
-								<!-- Menu Footer-->
 								<li class="user-footer">
 									<div class="pull-left">
 										<a href="#" class="btn btn-default btn-flat">Profile</a>
@@ -226,7 +147,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 								</li>
 							</ul>
 						</li>
-						<!-- Control Sidebar Toggle Button -->
 						<li>
 							<a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
 						</li>
@@ -234,32 +154,20 @@ scratch. This page gets rid of all links and provides the needed markup only.
 				</div>
 			</nav>
 		</header>
-		<!-- Left side column. contains the logo and sidebar -->
 		<aside class="main-sidebar">
-
-			<!-- sidebar: style can be found in sidebar.less -->
 			<section class="sidebar">
-
-				<!-- Sidebar user panel (optional) -->
 				<div class="user-panel">
 					<div class="pull-left image">
 						<img src="resources/BG_du_XML.jpg" class="img-circle" alt="User Image">
 					</div>
 					<div class="pull-left info">
 						<p>Arthur & Gautier</p>
-						<!-- Status -->
 						<a href="#"><i class="fa fa-circle text-success"></i> Online</a>
 					</div>
 				</div>
-
 				<br/>
-
-				<!-- /.search form -->
-
-				<!-- Sidebar Menu -->
 				<ul class="sidebar-menu">
 					<li class="header">Catégories</li>
-					<!-- Optionally, you can add icons to the links -->
 					<li class="treeview">
 						<a href="#"><i class="fa fa-link"></i> <span>Etudiants</span> <i class="fa fa-angle-left pull-right"></i></a>
 						<ul class="treeview-menu">
@@ -293,10 +201,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 			</section>
 			<!-- /.sidebar -->
 		</aside>
-
-		<!-- Content Wrapper. Contains page content -->
 		<div class="content-wrapper">
-			<!-- Content Header (Page header) -->
 			<section class="content-header">
 				<h1>
 					Bienvenue
@@ -307,15 +212,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
 					<li class="active">Scolarité</li>
 				</ol>
 			</section>
-
-			<!-- Main content -->
-			<section class="content"> 
-				<!-- <div class="container" id="main">
-					Main ici 
-				</div>  -->
+			<section class="content">
 				<div class="row">
 					<div class="col-lg-3 col-xs-6">
-						<!-- small box -->
 						<div class="small-box bg-green">
 							<div class="inner">
 								<h3><span id="pourcentageOfStudentsAtLeastOneDebt"></span><sup style="font-size: 20px">%</sup></h3>
@@ -328,7 +227,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 						</div>
 					</div><!-- ./col -->
 					<div class="col-lg-3 col-xs-6">
-						<!-- small box -->
 						<div class="small-box bg-yellow">
 							<div class="inner">
 								<h3 id="numberOfStudents"></h3>
@@ -341,7 +239,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 						</div>
 					</div><!-- ./col -->
 					<div class="col-lg-3 col-xs-6">
-						<!-- small box -->
 						<div class="small-box bg-red">
 							<div class="inner">
 								<h3 id="numberOfSubjects"></h3>
@@ -354,7 +251,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
 						</div>
 					</div><!-- ./col -->
 					<div class="col-lg-3 col-xs-6">
-						<!-- small box -->
 						<div class="small-box bg-yellow">
 							<div class="inner">
 								<h3 id="numberOfTeachers"></h3>
@@ -368,25 +264,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
 					</div><!-- ./col -->
 				</div>
 			</section> <!-- /.content -->
-		</div><!-- /.content-wrapper -->
-
-		<!-- Main Footer -->
+		</div>
 		<footer class="main-footer">
-			<!-- To the right -->
 			<div class="pull-right hidden-xs">
 				Appelez Arthur & Gautier dès maintenant pour profiter de leur expertise.
 			</div>
-			<!-- Default to the left -->
 			<strong>Copyright &copy; 2015 <a href="#">INSA Toulouse</a>.</strong> All rights reserved.
 		</footer>
-
-		<!-- Control Sidebar -->
 		<aside class="control-sidebar control-sidebar-dark">
-			<!-- Create the tabs -->
 			<ul class="nav nav-tabs nav-justified control-sidebar-tabs">
 				<li class="active"><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
 			</ul>
-			<!-- Tab panes -->
 			<div class="tab-content">
 				<div class="tab-pane active" id="control-sidebar-settings-tab">
 					<form method="POST">
@@ -399,29 +287,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
 							<p>
 								This will reload all the database.
 							</p>
-						</div><!-- /.form-group -->
+						</div>
 					</form>
-				</div><!-- /.tab-pane -->
+				</div>
 			</div>
-		</aside><!-- /.control-sidebar -->
-      <!-- Add the sidebar's background. This div must be placed
-      immediately after the control sidebar -->
+		</aside>
       <div class="control-sidebar-bg"></div>
-  </div><!-- ./wrapper -->
-
-  <!-- REQUIRED JS SCRIPTS -->
-  <!-- jQuery 2.1.4 -->
+  </div>
   <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
-  <!-- Bootstrap 3.3.5 -->
   <script src="bootstrap/js/bootstrap.min.js"></script>
-  <!-- AdminLTE App -->
   <script src="dist/js/app.min.js"></script>
-  <!-- Our JavaScript -->
   <script src="js/site.js"></script>
-
-    <!-- Optionally, you can add Slimscroll and FastClick plugins.
-         Both of these plugins are recommended to enhance the
-         user experience. Slimscroll is required when using the
-         fixed layout. -->
-     </body>
-     </html>
+</body>
+</html>
